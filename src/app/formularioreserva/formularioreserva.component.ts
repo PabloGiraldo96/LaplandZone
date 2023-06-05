9e12d77a521c535cd7ce0a211206fb4088f2c851
Juan Pablo Jaramillo Giraldo <juanpablogiraldo9602@gmail.com>
2023-06-04 20:33:50 -0500

    se agrega la magia y se agrega el metodo post en las reservas para escribir sobre la bd

--- src/app/formularioreserva/formularioreserva.component.ts
+++ src/app/formularioreserva/formularioreserva.component.ts
@@ -2,7 +2,7 @@ import { Component } from '@angular/core';
 import {FormGroup, FormBuilder} from '@angular/forms'
 import { Reserva } from '../interfaces/Reserva';
 import { ReservasService } from '../services/reservas.service';
-
+import { HabitacionesService } from '../services/habitaciones.service';
 
 @Component({
   selector: 'app-formularioreserva',
@@ -13,9 +13,19 @@ export class FormularioreservaComponent {
 
 public formulario: FormGroup
 public datosFormulario: any[] = []
+public habitaciones: any
 
-public constructor(public constructorForm: FormBuilder, public servicio: ReservasService){
+
+public constructor(public constructorForm: FormBuilder, public servicio: ReservasService, public servicioHabitacion: HabitacionesService){
 this.formulario = this.inicializarForm()
+this.servicioHabitacion.buscarHabitaciones().subscribe({
+  next: (respuesta) => { 
+    this.habitaciones= respuesta.habitaciones
+    console.log(respuesta)
+}, error: (Error)=> {
+    console.log(Error)
+}
+})
 }
 
 public inicializarForm(): FormGroup{
@@ -35,8 +45,13 @@ public inicializarForm(): FormGroup{
 public procesarDatos(): void {
   let datos = this.formulario.value
 
-  this.servicio.registrarReserva(datos).subscribe((respuesta)=>{
-    console.log(respuesta)
-   })
+  this.servicio.registrarReserva(datos).subscribe({
+  next: (respuesta) => { 
+    location.reload()
+  }, 
+    error: (Error) => {
+      console.log(Error)
+      }
+    })
   }
 }
